73ada093d87aa1a2fb581c87d3f4e1537d9fde44
LeeTwentyThree <31892011+LeeTwentyThree@users.noreply.github.com>
2020-11-20 13:39:38 -0500

    V1.2.0

---
+++ WindTurbineMod/WindTurbineConfig.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindTurbinesMod
+{
+    [Serializable]
+    public struct WindTurbineConfig
+    {
+        public bool PositionInfluencesPower;
+        public bool TurbineTakesDamage;
+        public bool TurbineMakesNoise;
+    }
+}
